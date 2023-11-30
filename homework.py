5d95a1ddaa0fac37667189ddf8b6ae84b4771603
Тимур Зырянов <zyryanovtimur1@gmail.com>
2023-11-30 21:39:25 +0300

    Сделал, должно работать.

--- homework.py
+++ homework.py
@@ -4,13 +4,17 @@ from typing import List, Union
 class InfoMessage:
     """Информационное сообщение о тренировке."""
 
-    def __init__(self, training_type: str, duration: float, distance: float,
-                 speed: float, calories: float):
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float):
+        self.training_type: str = training_type
+        self.distance: float = distance
+        self.duration: float = duration
+        self.speed: float = speed
+        self.calories: float = calories
 
     def get_message(self) -> str:
         return (
